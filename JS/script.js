be475669d2ac2d2c4ac3e990924355e944542039
Med Reda <elarabi.mohamede.reda@gmail.com>
2025-04-27 17:49:26 +0100

    Day 3
    Hid scrollbar
    
    added Date
    
    designed Add-Task button
    
    made task-num dynamic
    
    fixed no task issue (when loading the page without having a task, "no task..." won't appear.)

--- JS/script.js
+++ JS/script.js
@@ -1,37 +1,51 @@
+let date = new Date()
+let day = document.querySelector(".day-short")
+let month = document.querySelector(".month-short")
+let day_num = document.querySelector(".day-num")
+let year = document.querySelector(".year")
 let list = document.querySelector(".list")
 let no_task = document.querySelector(".no-task")
 let task_checkbox = document.querySelectorAll(".checkbox")
 let trashs = document.querySelectorAll(".trash")
+let tasks_num = document.querySelector(".tasks-num")
 
-function tasks_num_check(){
-    if(list.children.length===0){
+day.textContent = `${date.toLocaleDateString("default", { weekday: 'short' })},`
+month.textContent = `${date.toLocaleDateString("default", { month: 'short' })}`
+day_num.textContent = `${date.getDate()},`
+year = date.getFullYear()
+
+function tasks_num_check() {
+    tasks_num.textContent = `${list.children.length} Tasks`
+    if (list.children.length === 0) {
         list.style.display = "none"
         no_task.style.display = "flex"
         setTimeout(
-            function(){
+            function () {
                 no_task.style.opacity = "100"
             },
-        200)
+            200)
     }
 }
 
-trashs.forEach((trash)=>{
-    trash.addEventListener("click",()=>{
+tasks_num_check()
+
+trashs.forEach((trash) => {
+    trash.addEventListener("click", () => {
         let the_task = trash.closest(".task")
         the_task.style.opacity = "0"
         setTimeout(
-            function(){
+            function () {
                 trash.closest(".task").remove()
                 tasks_num_check()
             },
-        200)
+            200)
     })
 })
 
-task_checkbox.forEach((checkbox)=>{
-    checkbox.addEventListener("click",()=>{
-        let checkbox_check = checkbox.getAttribute("data-checked")==="true"
-        checkbox.setAttribute("data-checked",!checkbox_check)
+task_checkbox.forEach((checkbox) => {
+    checkbox.addEventListener("click", () => {
+        let checkbox_check = checkbox.getAttribute("data-checked") === "true"
+        checkbox.setAttribute("data-checked", !checkbox_check)
         let task = checkbox.closest(".task")
         let content = task.querySelector(".content")
         content.style.textDecoration = !checkbox_check ? "line-through" : "none";
